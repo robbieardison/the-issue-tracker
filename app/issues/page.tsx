d4eb65f1fb2a34b789aeaa366d329b92b274490e
Robby Ardison <101854553+robbieardison@users.noreply.github.com>
2023-10-29 10:40:25 +0700

    Customized radix

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,9 +3,9 @@ import { Button } from '@radix-ui/themes';
 import Link from 'next/link';
 
 const IssuesPage = () => {
-  return (
-    <div><Button><Link href='/issues/new'>New Issue</Link></Button></div>
-  )
+    return (
+        <div><Button><Link href='/issues/new'>New Issue</Link></Button></div>
+    )
 }
 
 export default IssuesPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
         <Theme appearance="light" accentColor="iris">
           <NavBar />
           <main className='p-5'>{children}</main>
-          <ThemePanel />
         </Theme>
       </body>
     </html>
